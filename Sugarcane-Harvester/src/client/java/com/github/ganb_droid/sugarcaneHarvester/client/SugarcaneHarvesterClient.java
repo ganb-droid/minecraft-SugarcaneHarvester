cb16908b2d06154384e3d2ce47ae0eeeca3db01c
ganb-droid <777+ganbdroid@users.noreply.github.com>
2025-12-14 19:43:27 +0900

    プロジェクトひな形作成

---
+++ Sugarcane-Harvester/src/client/java/com/github/ganb_droid/sugarcaneHarvester/client/SugarcaneHarvesterClient.java
@@ -0,0 +1,10 @@
+package com.github.ganb_droid.sugarcaneHarvester.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SugarcaneHarvesterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ Sugarcane-Harvester/src/main/java/com/github/ganb_droid/sugarcaneHarvester/SugarcaneHarvester.java
@@ -0,0 +1,10 @@
+package com.github.ganb_droid.sugarcaneHarvester;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SugarcaneHarvester implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
